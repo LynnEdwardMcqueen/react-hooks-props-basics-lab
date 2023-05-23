e29cc898304073e50c156c7b8af44370062fae4e
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-05-23 06:53:31 -0600

    Fixed a misunderstanding with Links props

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,9 +3,10 @@ import React from "react";
 function Links(props) {
     return (
       <div>
-        <a href= {props.links.github}>{props.links.github}</a>
-        <a href= {props.links.linkedin}>{props.links.linkedin}</a>
-      </div> 
+        <h3>Links</h3>
+        {props.github ? <a href= {props.github}>{props.github}</a> : null }
+        {props.linkedin ? <a href= {props.linkedin}>{props.linkedin}</a> : null }
+      </div>
     )
 }
 
